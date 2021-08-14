8aada275d50b17ded82f8b6ab544db19a8e6209d
Vitaliy Pavlenko <vpavlenko@iponweb.net>
2021-08-14 21:38:11 +0300

    use get_object_or_404 for getting an object

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.db.models import Count
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from blog.models import Post, Tag
 
 
@@ -53,7 +53,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.annotate(likes_count=Count('likes')).get(slug=slug)
+    post = get_object_or_404(Post.objects.annotate(likes_count=Count('likes')), slug=slug)
     comments = post.comments.prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -90,7 +90,7 @@ def post_detail(request, slug):
 
 
 def tag_filter(request, tag_title):
-    tag = Tag.objects.get(title=tag_title)
+    tag = get_object_or_404(Tag, title=tag_title)
 
     most_popular_tags = Tag.objects.popular()[:5]
 
